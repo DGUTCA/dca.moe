4fad9d92903fe4f44b052ff0d03e10419686cbda
ssrinke <ssrinke@gmail.com>
2023-10-03 19:33:10 +0800

    refactor: nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,15 +5,15 @@ import { usePathname } from 'next/navigation'
 
 export default function Nav() {
     const pathname = usePathname()
-    function isActived(path: string) {
-        return pathname.startsWith(path) ? "tab tab-active" : "tab"
-    }
-    function isHome() {
-        if (pathname === "/"){
-            return "tab tab-active"
-        }else{
-            return "tab"
+    const navs = [
+        { name: "Home", path: "/" },
+        { name: "Events", path: "/events" },
+    ]
+    function navClass(path: string) {
+        if (path === "/") {
+            return pathname === path ? "tab tab-active" : "tab"
         }
+        return pathname.startsWith(path) ? "tab tab-active" : "tab"
     }
     return (
         <div className="navbar bg-base-100 top-0 left-0 right-0 fixed z-50">
@@ -26,16 +26,26 @@ export default function Nav() {
                 </div>
                 <div className="flex-none hidden md:block">
                     <div className="tabs uppercase">
-                        <Link href="/" className={isHome()+" tab-bordered"}>Home</Link>
-                        <Link href="/events" className={isActived("/events")+" tab-bordered"}>Events</Link>
+                        {navs.map((data, idx) => {
+                            return (
+                                <div key={idx}>
+                                    <Link href={data.path} className={navClass(data.path)+" tab-bordered"}>{data.name}</Link>
+                                </div>)
+                        })}
                     </div>
                 </div>
                 <div className="flex-none md:hidden">
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn m-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg></label>
                         <ul tabIndex={0} className="dropdown-content z-[1] tabs tabs-boxed shadow bg-base-100 rounded-box">
-                            <li><Link href="/" className={isHome()}>Home</Link></li>
-                            <li><Link href="/events" className={isActived("/events")}>Events</Link></li>
+                            {navs.map((data, idx) => {
+                                return (
+                                    <li>
+                                        <div key={idx}>
+                                            <Link href={data.path} className={navClass(data.path)}>{data.name}</Link>
+                                        </div>
+                                    </li>)
+                            })}
                         </ul>
                     </div>
                 </div>
