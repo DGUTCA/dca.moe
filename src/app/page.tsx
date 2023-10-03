91ac1e936ed63ed9a4cdcf18c946ceadb610847e
ssrinke <ssrinke@gmail.com>
2023-10-03 20:48:02 +0800

    style: service(remove px-8)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,7 @@ export default function Home() {
     return (
         <div className="md:container px-6 prose">
             <h2># 线上服务</h2>
-            
-            <div className="px-8">
+            <div>
                 <div className="grid grid-flow-row grid-cols-2 md:grid-cols-4 gap-5">
                     <Service img_path="/icons/fix.svg" title="报修平台" desc="为广大师生同学提供设备维修服务喵"></Service>
                     <Service img_path="/icons/calendar.svg" title="活动日历" desc="丰富的活动在这里等你喵"></Service>
@@ -17,7 +16,6 @@ export default function Home() {
                     <Service img_path="/icons/issue.svg" title="问题反馈" desc="如果您有任何问题请及时向我们反馈喵"></Service>
                 </div>
             </div>
-            
             <div className="grid grid-flow-row md:grid-cols-2">
                 <div className="grid"> 
                     <h2># 最近文章</h2>
@@ -29,15 +27,11 @@ export default function Home() {
                     </Abstarcts>
 
                 </div>
-
                 <div className="grid">
                     <h2># 最新活动</h2>
 
                 </div>
-
-
             </div>
-
         </div>   
     )
 }
