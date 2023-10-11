f5c8a46a068ce7320329e4abafb3d893186f9f15
ssrinke <ssrinke@gmail.com>
2023-10-11 10:53:03 +0800

    style: service

--- src/components/service.tsx
+++ src/components/service.tsx
@@ -1,21 +1,29 @@
-import Image from "next/image"
+import Image from "next/image";
 
 interface ServiceInterface {
-    img_path: string
-    title: string
-    desc?: string
-    className?: string
+  img_path: string;
+  title: string;
+  desc?: string;
+  className?: string;
 }
 
 const Service: React.FC<ServiceInterface> = (props) => {
-    return (
-        <div className={`${props.className} grid rounded-3xl border-black p-5 service shadow-lg place-items-center`}>
-            <Image className="not-prose" width={100} height={100}  src={props.img_path} alt=""/>
-            <h3>{props.title}</h3>
-            <h5>{props.desc}</h5>
-        </div>
-    )
-}
+  return (
+    <div className="service rounded-3xl border-black p-5 shadow-lg">
+      <div className={`${props.className} grid place-items-center`}>
+        <Image
+          className="not-prose"
+          width={100}
+          height={100}
+          src={props.img_path}
+          alt=""
+        />
+        <h3>{props.title}</h3>
+        <h5>{props.desc}</h5>
+      </div>
+    </div>
+  );
+};
 
-export default Service
-export type {ServiceInterface}
+export default Service;
+export type { ServiceInterface };
